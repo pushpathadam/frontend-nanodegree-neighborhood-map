b425948fbffb8e621be6e3b683d68a6fe2707407
pushpathadam <tom@verycleverdesign.com>
2015-09-29 22:26:55 -0700

    Add geocode

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
-var map;    // declares a global map variable
-/*
-Start here! initializeMap() is called when page is loaded.
-*/
+var geocoder;
+var map;
+
 function initMap() {
+
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(34.1592, -118.5003);
 
     var locations;
 
     var mapOptions = {
         center: {lat: 34.1592, lng: -118.5003},
         zoom: 13,
+        //center: latlng,
         //disableDefaultUI: true
     };
 
     // This next line makes `map` a new Google Map JavaScript Object and attaches it to
     // <div id="map">, which is appended as part of an exercise late in the course.
-    map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+    map = new google.maps.Map(document.getElementById('map'),mapOptions);
 
     // Adjusted color of the map to a more monochrome look
     map.set('styles',[
@@ -32,11 +35,26 @@ function initMap() {
         }
     ]);
 
-
-
     // Sets the boundaries of the map based on pin locations
     //window.mapBounds = new google.maps.LatLngBounds();
 
       // locations is an array of location strings returned from locationFinder()
-
 }
+
+
+function codeAddress(){
+    var address = document.getElementById("address").value;
+    console.log(address);
+    console.log(geocoder);
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+};
